576cd7fb9e7abef285f87c76929619aa840836aa
pantsel <panagis.tselentis@konghq.com>
2024-10-23 22:40:45 +0200

    ref

--- k6/spike-test.js
+++ k6/spike-test.js
@@ -4,8 +4,8 @@ import { check, sleep } from 'k6';
 export let options = {
     stages: [
         { duration: '10s', target: 0 },    // Start with 0 users
-        { duration: '10s', target: 1000 }, // Spike to 1000 users in 10 seconds
-        { duration: '30s', target: 1000 }, // Hold at 1000 users for 30 seconds
+        { duration: '10s', target: 500 }, // Spike to 500 users in 10 seconds
+        { duration: '30s', target: 500 }, // Hold at 500 users for 30 seconds
         { duration: '10s', target: 0 },    // Drop back to 0 users
     ],
     thresholds: {
